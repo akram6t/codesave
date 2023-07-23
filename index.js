bf8ab495ef90b6021c0e98f8bcc0a79f046625b1
akram6t <khanakram8435@gmail.com>
2023-07-23 22:59:07 +0530

    last commit

--- index.js
+++ index.js
@@ -33,10 +33,8 @@ app.get("/apis/users", (req, res) => {
 // app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.use(express.static("./client"));
-if (process.env.NODE_ENV == "production") {
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname + './client', 'index.html'));
 })
-}
 
 app.use('/api', router);
