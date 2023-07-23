1ec0fea3148aeda0b4dff609dd5903a273b89631
akram6t <khanakram8435@gmail.com>
2023-07-23 23:05:12 +0530

    commits

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ app.get("/apis/users", (req, res) => {
 // app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.use(express.static("./client"));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + './client', 'index.html'));
-})
+app.get("*", (req, res) => {
+  res.json("hellow worolds");
+});
 
 app.use('/api', router);
