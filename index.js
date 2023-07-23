b9d87efbfbad85071833915caeabc20e58074462
akram6t <khanakram8435@gmail.com>
2023-07-23 22:43:04 +0530

    change folder name

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ app.get("/apis/users", (req, res) => {
 
 // app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
-app.use(express.static("./public"));
+app.use(express.static("./client"));
 if (process.env.NODE_ENV == "production") {
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname + './public', 'index.html'));
+  res.sendFile(path.resolve(__dirname + './client', 'index.html'));
 })
 }
 
