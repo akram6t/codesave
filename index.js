fe1cde573b056d0e8366401a690a2a6ee132b6c4
akram6t <khanakram8435@gmail.com>
2023-07-23 22:31:11 +0530

    commit

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.get("/apis/users", (req, res) => {
 
 app.use(express.static(path.join(__dirname, './public')));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.use('/api', router);
